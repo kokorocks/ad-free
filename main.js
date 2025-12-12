8a15e39248d84310f2b607a673ffb421f85c672a
Ethan <175902687+kokorocks@users.noreply.github.com>
2025-12-12 18:22:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -266,3 +266,14 @@ downloadBtn.onclick = async () => {
             document.body.appendChild(menu);
         });
 })();
+
+//-------------------------------------------------------- skipcut youtube ad remover ----------------------------------------------//
+
+(function() {
+    if(window.location.hostname==='www.youtube.com' || window.location.hostname==='youtube.com' || window.location.hostname==='youtu.be'){
+        url=window.location.href.replace('youtube', 'skipcut')
+        location.replace(url)
+    }else{
+        alert('no')
+    }
+})()
