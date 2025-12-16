3dc263b48d77ac6a6883cc859d5fad61cbdb5c3b
Ethan <175902687+kokorocks@users.noreply.github.com>
2025-12-16 14:39:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -413,3 +413,10 @@ downloadBtn.onclick = async () => {
     dialog.addEventListener('click', e => e.stopPropagation());
     cls.onclick=function(){overlay.outerHTML=''}
 })();
+//-------------------------------------------------------------auto darkmode------------------------------------------------//
+(function() {
+var script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/darkreader/darkreader.js';
+script.onload = () => DarkReader.enable({ brightness: 100, contrast: 90, sepia: 10 });
+document.head.appendChild(script);
+})();
